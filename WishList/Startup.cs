bde63184164763aae9cad02521c502f56212e635
Oliver Franz <Oliver@bca.de>
2021-04-20 12:43:22 +0200

    Config Grundeinstellungen

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -11,21 +11,32 @@ using Microsoft.Extensions.Hosting;
 
 namespace WishList
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.Run(async (context) =>
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if(env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+				app.UseDeveloperExceptionPage();
+            }
+			else
+            {
+				app.UseExceptionHandler("/Home/Error");
+            }
+				 
+			app.UseRouting();
+			app.UseEndpoints(endpoints => 
+			{ 
+				endpoints.MapDefaultControllerRoute(); 
+			});
+		}
+	}
 }
